681433587b3306d1885689f6a6d9433bd72b898f
vorlefan <dax-soft@live.com>
2022-12-27 14:19:03 -0300

    test json

---
+++ src/__tests__/json.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from 'vitest';
+
+import { default as PathRoute } from '../path/index';
+
+const TestRoute = new PathRoute();
+TestRoute.add('main', __dirname);
+TestRoute.add('@', TestRoute.backward('main', 2));
+
+const FILENAME_TEST = '__vite.test.json';
+const FILENAME_DATA_TEST = {
+    x: 1,
+    y: 5,
+};
+
+describe('PathJsonManager', () => {
+    it('read', async () => {
+        const packageJsonFilepath = TestRoute.plug('@', 'package.json');
+        const read = await TestRoute.json().read(packageJsonFilepath || '');
+        expect(read?.name).toBe('@vorlefan/path');
+    });
+
+    it('write', async () => {
+        const write = await TestRoute.json().write(
+            TestRoute.plug('main', FILENAME_TEST) || '',
+            FILENAME_DATA_TEST
+        );
+        expect(write).toBe(true);
+    });
+
+    it('read and clean', async () => {
+        const filepath = TestRoute.plug('main', FILENAME_TEST) || '';
+        const read = await TestRoute.json().read(filepath);
+        expect(read?.x + read?.y).toBe(6);
+
+        const clean = await TestRoute.io().remove(filepath);
+        expect(clean).toBe(true);
+    });
+});
