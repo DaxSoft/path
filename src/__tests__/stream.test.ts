2de108945f443a7231ec394d3120604189232b4b
vorlefan <dax-soft@live.com>
2022-12-27 14:25:29 -0300

    PathStreamManager

--- src/__tests__/stream.test.ts
+++ src/__tests__/stream.test.ts
@@ -4,23 +4,30 @@ import { default as PathRoute } from '../path/index';
 
 const TestRoute = new PathRoute();
 TestRoute.add('main', __dirname);
-TestRoute.add('@', TestRoute.backward('main', 2));
 
-const FILENAME_TEST = '__vite.test.json';
-const FILENAME_DATA_TEST = {
-    x: 1,
-    y: 5,
-};
+const FILENAME_TEST = '__vite.test.stream.txt';
+const FILENAME_DATA_TEST = '@vorlefan';
+const URL_DOWNLOAD = 'https://avatars.githubusercontent.com/u/6865744?v=4';
 
 describe('PathStreamManager', () => {
-    it('read', async () => {
-        const packageJsonFilepath = TestRoute.plug('@', 'package.json');
-        const read = await TestRoute.json().read(packageJsonFilepath || '');
-        expect(read?.name).toBe('@vorlefan/path');
+    it('download', async () => {
+        const dest = TestRoute.plug('main', 'github_profile.png') || '';
+        const download = await TestRoute.stream().download(
+            URL_DOWNLOAD,
+            dest,
+            'https'
+        );
+        expect(download).toBe(true);
+
+        const hasFile = await TestRoute.io().isFileValid(dest);
+        expect(hasFile).toBe(true);
+
+        const clean = await TestRoute.io().remove(dest);
+        expect(clean).toBe(true);
     });
 
     it('write', async () => {
-        const write = await TestRoute.json().write(
+        const write = await TestRoute.stream().write(
             TestRoute.plug('main', FILENAME_TEST) || '',
             FILENAME_DATA_TEST
         );
@@ -29,8 +36,8 @@ describe('PathStreamManager', () => {
 
     it('read and clean', async () => {
         const filepath = TestRoute.plug('main', FILENAME_TEST) || '';
-        const read = await TestRoute.json().read(filepath);
-        expect(read?.x + read?.y).toBe(6);
+        const read = (await TestRoute.stream().read(filepath)) || '';
+        expect(/^@/.test(read)).toBe(true);
 
         const clean = await TestRoute.io().remove(filepath);
         expect(clean).toBe(true);
